dd3b16597c28ba25d647b5444079a16dcecc22bc
saindan12 <gangulasainandan@gmail.com>
2023-11-23 19:37:31 +0530

    add entire directory structure 1

--- idsrestapi/idsapi/views.py
+++ idsrestapi/idsapi/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
+from .models import Network
 
-# Create your views here.
+@csrf_exempt
+def analyze_logs(request):
+    if request.method == 'POSt':
+        try:
+            received_data = json.loads(request.body)
+            logs = received_data.get('logs',[])
+            
+            threat_logs = []
+            
+            for log_data in logs:
+                if log_data.get('source_ip') == '10.0.0.1':
+                    threat_logs.append(log_data)
+                    
+            response_data ={
+                'message': 'log analysis complete',
+                'threat_logs' : threat_logs
+            }
+            
+            return JsonResponse(response_data, status=200)
+        
+        except json.JSONDecodeError as e:
+            return JsonResponse({'error': 'Invalid Json format'}, status=400)
+    
+    return JsonResponse({'error': "only POST requests are allowed"}, status=405)
+
+
