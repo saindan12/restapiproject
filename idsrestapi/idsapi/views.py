b188d6fa718a3d1d86dc054c86e057bbb76e06e9
saindan12 <gangulasainandan@gmail.com>
2023-11-24 00:39:43 +0530

    add entire directory structure 1

--- idsrestapi/idsapi/views.py
+++ idsrestapi/idsapi/views.py
@@ -9,7 +9,7 @@ def analyze_logs(request):
     if request.method == 'POST':
         try:
             received_data = json.loads(request.body)
-            logs = received_data.get('logs',[])
+            logs = received_data.get('logs', [])
             
             threat_logs = []
             
@@ -23,12 +23,11 @@ def analyze_logs(request):
                 if log_data.get('source_ip') == '10.0.0.1':
                     threat_logs.append(network_instance)
                 
-                serializer = NetworkSerializer(threat_logs, many=True)
-                serialized_logs = serializer.data
-                    
-            response_data ={
+            serializer = NetworkSerializer(threat_logs, many=True)  # Serializer should cover the entire list
+            
+            response_data = {
                 'message': 'log analysis complete',
-                'threat_logs' : threat_logs
+                'threat_logs': serializer.data  # Use the serialized data here
             }
             
             return JsonResponse(response_data, status=200)
